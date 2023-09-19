31ec420e44b63479c17ca0487da812ead24d0542
AlphaHandia <118402552+AlphaHandia@users.noreply.github.com>
2023-09-19 11:18:07 +0200

    implementation footer

---
+++ Kasa/src/components/Footer/footer.jsx
@@ -0,0 +1,19 @@
+import '../../Sass/Base/Footer.scss'
+
+import styled from 'styled-components'
+import D_Footer from'../../assets/image/D_Footer.png'
+
+
+const FootLogoText = styled.img`
+`
+function Footer() {
+    return (
+      <div className='FooterContainer'>
+       
+        <FootLogoText src={D_Footer}/>
+        
+      </div>
+    )
+  }
+
+  export default Footer
--- Kasa/src/index.js
+++ Kasa/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './Sass/main.css';
 import Error from './components/Error/error';
 import Header from './components/header/header';
+import Footer from './components/Footer/footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,9 @@ root.render(
     <Routes>
     <Route path='*' element={<Error/>}/>
     </Routes>
+    <Footer />
   </Router> 
+  
 </React.StrictMode>
 );
 
